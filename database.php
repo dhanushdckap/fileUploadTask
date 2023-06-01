733dbcfd5219f01fefd941d2e4ba34ccfcd8b435
DCKAP-L150 <dhanushgdckap@gmail.com>
2023-06-01 15:32:15 +0530

    file path is completed.

--- database.php
+++ database.php
@@ -9,8 +9,8 @@ $conn = mysqli_connect($servername, $username, $password,"$dbname");
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  // die("Connection failed: " . $conn->connect_error);
 
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 ?>
